bc49e12202634aa9ac0121c93d94ddc41234df60
jfitzsimmons <jpfitz501@gmail.com>
2021-11-08 03:01:36 +0000

    Big update- Fixed photoGallery, added JavaScript directory, Wen is screaming for some reason, basic JS alert on main page

---
+++ JavaScript/My_Alert_File.js
@@ -0,0 +1,5 @@
+//This is a comment
+message="Hey, just wanted you to know that this site is still very much a work in progress! So, if it looks a little rough don't judge me too much! \n -John";
+alert(message);
+console.log("Why are you inspecting my site? Can't you just leave me alone?"); 
+
